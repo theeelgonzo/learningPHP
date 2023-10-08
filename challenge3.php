d2c077804e1fe5aa4a141f80ff1a1a7a9d512caf
Jordan Godwin <jordan.godwin13@gmail.com>
2023-10-08 09:59:30 -0500

    Solve Challenge 3.

---
+++ challenge3.php
@@ -0,0 +1,10 @@
+<?php
+$a = (14 + 82 - 32 / 2) ** 2;
+echo $a . "\n";
+$b = 18 * (3 / 6 - 9) * 10;
+echo $b . "\n";
+$c = 5 * (12 / 2 - 8 * 4 + 12 * 6);
+echo $c . "\n";
+
+//$results = array($a, $b, $c);
+print_r( $results = array($a, $b, $c));
