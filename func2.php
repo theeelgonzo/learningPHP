88f3fc059c87c358d486b72361e736a3931adde0
Jordan Godwin <jordan.godwin13@gmail.com>
2023-10-16 18:41:46 -0500

    Add function with default or optional arguments.

--- func2.php
+++ func2.php
@@ -2,9 +2,18 @@
 
 function double(int | float $a) {
     return $a * 2;
+    echo "\n";
 
 }
 
 echo double(3);
 //echo double("apple");
 echo double(3.4);
+
+function multiplier( $a, $multiplier = 2) {
+    return $a * $multiplier;
+    echo "\n";
+}
+
+echo multiplier(4);
+echo multiplier (4,4);
